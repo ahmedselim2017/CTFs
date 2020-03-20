d51fc39c018569cd61c12c4353d5b2482301b42d
ahmedselim2017 <ahmedselimuzum@gmail.com>
2020-03-20 18:41:07 +0300

    hackthebox invitation

---
+++ hackthebox/invitation/inviteapi.js
@@ -0,0 +1,31 @@
+function verifyInviteCode(code) {
+    var formData = {
+        "code": code
+    };
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        data: formData,
+        url: '/api/invite/verify',
+        success: function(response) {
+            console.log(response)
+        },
+        error: function(response) {
+            console.log(response)
+        }
+    })
+}
+
+function makeInviteCode() {
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: '/api/invite/how/to/generate',
+        success: function(response) {
+            console.log(response)
+        },
+        error: function(response) {
+            console.log(response)
+        }
+    })
+}
